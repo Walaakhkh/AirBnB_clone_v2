26d71151689f2356bbe029ce17a275ce4c1e193b
Walaaakhkh <Walaakhalil98@gmail.com>
2024-07-29 16:22:36 -0700

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+"""
+This script starts a Flask web application that listens on 0.0.0.0, port 5000.
+The root route ("/") displays "Hello HBNB!".
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
-def hello():
+def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 if __name__ == "__main__":
--- web_flask/__init__.py
+++ web_flask/__init__.py
@@ -1,1 +1,4 @@
-# __init__.py
+#!/usr/bin/python3
+"""
+This is the initialization file for the web_flask module.
+"""
