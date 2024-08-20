8d2587ec67bb6a804181992449ae8d8b015f2637
Walaaakhkh <Walaakhalil98@gmail.com>
2024-08-20 07:19:28 -0700

    task 7

---
+++ web_flask/models/engine/file_storage.py
@@ -0,0 +1,35 @@
+import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+class FileStorage:
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self, cls=None):
+        # Your existing implementation
+        pass
+
+    def new(self, obj):
+        # Your existing implementation
+        pass
+
+    def save(self):
+        # Your existing implementation
+        pass
+
+    def reload(self):
+        # Your existing implementation
+        pass
+
+    def delete(self, obj=None):
+        # Your existing implementation
+        pass
+
+    def close(self):
+        self.reload()
