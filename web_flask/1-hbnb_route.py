4df703a6bb9a98546773ca92624aa7fd8a322dfc
Walaaakhkh <Walaakhalil98@gmail.com>
2024-08-20 05:44:02 -0700

    task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,10 +8,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
