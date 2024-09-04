96730280cd8b80574b1afbfb69a12921fffa1680
Walaakhkh <Walaakhalil98@gmail.com>
2024-09-04 14:49:18 +0000

    web flask

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,34 +1,28 @@
 #!/usr/bin/python3
 """
-This script starts a Flask web application that listens on 0.0.0.0, port 5000.
-The root route ("/") displays "Hello HBNB!".
-The "/hbnb" route displays "HBNB".
-The "/c/<text>" route displays "C " followed by the value of the text variable
-(with underscores replaced by spaces).
+starts a Flask web application
 """
-from flask import Flask, escape
 
+from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
-    return "Hello HBNB!"
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'"""
-    return "HBNB"
+    """returns HBNB"""
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
-    """Displays 'C ' followed by the value of the text variable, with
-    underscores replaced by spaces"""
-    text = escape(text).replace('_', ' ')
-    return f"C {text}"
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
